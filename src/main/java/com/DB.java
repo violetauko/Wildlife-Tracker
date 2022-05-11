d35148c42e8496969b98e6758ec674d053197c26
violetauko <aukoviolet24@gmail.com>
2022-05-11 11:04:49 +0300

    add rangerName

--- src/main/java/com/DB.java
+++ src/main/java/com/DB.java
@@ -3,7 +3,7 @@ package com;
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","violet","3519ella");
-//public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-164-40-66.compute-1.amazonaws.com:5432/d4ssuc9g1odff3","tngdnnalrwxdbh","5a073827c079753ac66748cb83d2e37a018447611fa0b7a6dff43d1de07c3bd0");
+    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","violet","3519ella");
+public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-164-40-66.compute-1.amazonaws.com:5432/d4ssuc9g1odff3","tngdnnalrwxdbh","5a073827c079753ac66748cb83d2e37a018447611fa0b7a6dff43d1de07c3bd0");
 
 }
